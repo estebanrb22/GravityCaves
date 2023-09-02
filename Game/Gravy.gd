ba5b7e8355c59cfa73515d4b4e4ea6e3e46dee47
estebanrb22 <esteban.romero.b@ug.uchile.cl>
2023-09-02 12:42:39 -0400

    Script for Gravy

---
+++ Game/Gravy.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+func _ready():
+	#Se ejecuta al abrir el juego
+	pass
+	
+func _process(delta):
+	#Se ejecuta en cada frame del juego, depende de la pantalla y no es constante :d
+	pass
+	
+func _physics_process(delta):
+	#Estabiliza la frecuencia de ejecución intentando que no dependa del computador
+	pass	
+	
+	
+	#Para salto ocupar
+	# press_Space:
+	#  if while_press:
+	#    activar salto mientras se mantiene para más precisión
+	#    con un contador	
