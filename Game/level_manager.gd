1abf402c8f75436022c3b02e7bec6d47b09cba08
fabitauwu <fabiana.ferrada@ug.uchile.cl>
2023-10-09 23:22:17 -0300

    LevelManager

---
+++ Game/level_manager.gd
@@ -0,0 +1,23 @@
+extends Node
+
+#@export var main_menu: PackedScene #Para el menú
+@export var credits: PackedScene
+@export var levels: Array[PackedScene]
+var current_level = 0
+func go_to_level(index):
+	if index < levels.size():
+		var level = levels[index]
+		if level:
+			current_level = index
+			get_tree().change_scene_to_packed(level)
+	
+func start_game():
+	go_to_level(0)
+
+func next_level():
+	go_to_level(current_level + 1)
+	
+#func go_to_main_menu():
+#	current_level = 0
+#	if main_menu:
+#		get_tree().change_scene_to_packed(main_menu)
---
+++ Game/puertita.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+@onready var label: Label = $Label
+var inside: Array[CharacterBody2D] = []
+
+func _ready():
+	label.hide()
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+	
+func _on_body_entered(body: Node):
+	inside.append(body)
+	if not label.visible:
+		label.show()
+		
+func _on_body_exited(body: Node):
+	inside.erase(body)
+	if inside.is_empty():
+		label.hide()
+		
+func _input(event):
+	if event.is_action_pressed("puertas"):
+		for body in inside:
+			LevelManager.next_level()
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
