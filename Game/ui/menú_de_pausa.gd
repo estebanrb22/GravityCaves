03f9483f35cef3b0cd4a13dccf997f7fd04eedd2
Guillineitor <guillermoandresgardacuevas@gmail.com>
2023-12-11 13:23:09 -0300

    menú de pausa en niveles

--- Game/ui/menú_de_pausa.gd
+++ Game/ui/menú_de_pausa.gd
@@ -1,9 +1,11 @@
 extends Control
 
-@onready var volver: TextureButton = $"MarginContainer/CenterContainer/VBoxContainer/Botón de Volver"
+@onready var volver: Button = $"MarginContainer/CenterContainer/VBoxContainer/Botón de Volver"
+@onready var continuar: Button = $"MarginContainer/CenterContainer/VBoxContainer/Botón de Continuar"
 
 func _ready() -> void:
 	volver.pressed.connect(_on_volver_pressed)
+	continuar.pressed.connect(_on_continuar_pressed)
 	visible = false
 	
 func _input(event):
@@ -13,4 +15,15 @@ func _input(event):
 		
 
 func _on_volver_pressed():
+	get_tree().paused = false 
 	get_tree().change_scene_to_file("res://Game/ui/main_menu.tscn")
+	
+func _on_continuar_pressed():
+	visible = not get_tree().paused
+	get_tree().paused = false 
+	
+
+
+
+	
+	
